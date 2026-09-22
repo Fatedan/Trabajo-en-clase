f076f866b9948471ca794d61f73906603130160e
Fatedan <dtorreslo@unal.edu.co>
2026-09-22 14:20:33 +0000

    falta que corra bien

--- actividad.cpp
+++ actividad.cpp
@@ -4,18 +4,48 @@
 using namespace std;
 int main()
 {
-int n, i=1;
-printf("ingrese el numero n");
+int n, i=1, j=0, m;
+printf("ingrese el numero n \n");
 scanf("%i",&n);
-contador: i++;
+
+final: cout<<"*"<<endl;
+goto evaluacion;
 Inicial: cout<<"*";
+goto evaluacion;
 Intermedio: cout <<" ";
+goto evaluacion;
+
+evaluacion:
 if (i<=n)
 {
-    i++;
-    goto Inicial;
-
+    if (j%(n)==0)
+    { 
+        if(j<(i*n))
+        {
+         j++;
+         goto Inicial;
+        }
+        else
+        {
+         j++;
+         i++;
+         goto final;
+        }
+        
+     }
+    else 
+    {
+        if (i%n==1)
+        {
+            j++;
+            goto Inicial;
+         }
+         else
+         {j++;
+        goto Intermedio;}
+    }
 } 
+
 
 
 
